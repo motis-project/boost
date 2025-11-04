876d43bd1bd9f327ab440d4796d2ab1c74f26803
Pablo Hoch <pablo@melaxis.com>
2025-11-04 11:13:14 +0100

    fix stacktrace build

--- strip.sh
+++ strip.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 find libs -path "*/doc/*" -delete
+find libs -type f -name 'Jamfile.v2' -delete
 find libs -type f \
           -not -path "*/src/*" \
           -not -path "*/cmake/*" \
           -not -path "*/config/*" \
+          -not -path "*/build/*" \
           -not -regex "libs/[^/]+/CMakeLists\.txt" \
           -delete
 find libs -empty -type d -delete
